3392675e8d48ca7c3e5444ebf1b067101f1ddf17
Neil Parikh <parikh.neil@me.com>
2016-02-27 11:18:50 -0500

    some skeleton code to support bluetooth over hardware serial

--- seek/seek.ino
+++ seek/seek.ino
@@ -3,11 +3,17 @@
 
 #define USE_SOFT_SER 1
 
-SoftwareSerial bluetooth(11,10); //RX TX (connect pin11(RX) to TX on the module.
+#if USE_SOFT_SER
+  SoftwareSerial bluetooth(11,10); //RX TX (connect pin11(RX) to TX on the module.
+#else
+  #define bluetooth Serial
+#endif
 //Servo MyServo;
 
 void setup() {
+#if USE_SOFT_SER
   Serial.begin(9600);
+#endif
   bluetooth.begin(9600);
   while (!bluetooth.available()) {
     Serial.print("bluetooth serial not available");

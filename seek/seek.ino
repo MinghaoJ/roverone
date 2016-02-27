af996ee833e7b5552422065f7db2ed4d22728824
Neil Parikh <parikh.neil@me.com>
2016-02-27 11:52:53 -0500

    skeleton code for main control loop

--- seek/seek.ino
+++ seek/seek.ino
@@ -8,6 +8,9 @@
 #else
   #define bluetooth Serial
 #endif
+
+void setLeftMotorTo(int speed);
+void setRightMotorTo(int speed);
 //Servo MyServo;
 
 void setup() {
@@ -27,15 +30,41 @@ void loop() {
     int command = bluetooth.read();
 
     switch (command) {
-      case 'a':
-        Serial.print("a was sent");
-        digitalWrite(13, HIGH);
+      case 'f': // move forward
+        // set both motors to go forwards at the same speed
+        setLeftMotorTo(0.5);
+        setRightMotorTo(0.5);
         break;
-
-      case 'b':
-        Serial.print("b was sent");
-        digitalWrite(13, LOW);
+      case 'b': // move backward
+        // set both motors to go backwards at the same speed
+        setLeftMotorTo(-0.5);
+        setRightMotorTo(-0.5);
+        break;
+      case 'r': // turn right
+        // set the right to go backwards at a speed x
+        setRightMotorTo(-0.5);
+        // set the left to go forwards at speed x
+        setLeftMotorTo(0.5);
+        break;
+      case 'l': // turn left
+        // set the right to go forwards at speed x
+        setRightMotorTo(0.5);
+        // set the left to go backwards at a speed x
+        setLeftMotorTo(-0.5);
         break;
     }
+    // sleep for n seconds
+    delay(500);
+    // set both motors to 0
+    setLeftMotorTo(0);
+    setRightMotorTo(0);
   }
+}
+
+void setLeftMotorTo(int speed) {
+
+}
+
+void setRightMotorTo(int speed) {
+
 }

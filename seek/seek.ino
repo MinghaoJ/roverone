0b87324a1b94d2f14794f045aaab3cd5ae3e554b
Neil Parikh <parikh.neil@me.com>
2016-02-27 10:30:44 -0500

    rename MySerial to bluetooth and clean up formatting

--- seek/seek.ino
+++ seek/seek.ino
@@ -1,41 +1,32 @@
 #include <SoftwareSerial.h>;
 #include <Servo.h>;
 
-
-SoftwareSerial MySerial(11,10) ;//RX TX (connect pin11(RX) to TX on the module.
+SoftwareSerial bluetooth(11,10); //RX TX (connect pin11(RX) to TX on the module.
 //Servo MyServo;
-#define USE_SOFT_SER 1 
+#define USE_SOFT_SER 1
 
-
-
-void setup()
-
-{
-    Serial.begin(9600);
-    MySerial.begin(9600);
-    while (!MySerial.available()) {
-      Serial.print("bluetooth serial not available");
-    }
-    Serial.print("bluetooth serial available");
-    //MyServo.attach(2); output to servo motor;
+void setup() {
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  while (!bluetooth.available()) {
+    Serial.print("bluetooth serial not available");
+  }
+  Serial.print("bluetooth serial available");
+  //MyServo.attach(2); output to servo motor;
 }
 
-void loop()
-{
-  if (MySerial.available())
-      {
-        int command = MySerial.read();
-      
-        if (command == 97) // 97 is the code for a
-        {
-        
-           Serial.print(" a ");
-           digitalWrite(13, HIGH);
-        }
-        if (command == 98) // 98 is the code for b
-        {
-            Serial.print(" b ");
-            digitalWrite(13, LOW);
-        }
+void loop() {
+  if (bluetooth.available()) {
+    int command = bluetooth.read();
+
+    if (command == 97) { // 97 is the code for a
+     Serial.print(" a ");
+     digitalWrite(13, HIGH);
     }
+
+    if (command == 98) { // 98 is the code for b
+      Serial.print(" b ");
+      digitalWrite(13, LOW);
+    }
+  }
 }
